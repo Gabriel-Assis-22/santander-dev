122b1ab187b81fb1ddbce7365854aab6d44323c4
Gabrie Roberto <152230629+Gabriel-Assis-22@users.noreply.github.com>
2025-08-26 16:08:25 -0300

    commit

---
+++ src/main/java/me/dio/domain/Account.java
@@ -0,0 +1,11 @@
+package me.dio.domain;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private long id;
+    private String number;
+    private String agency;
+    private BigDecimal balance;
+    private BigDecimal limit;
+}
---
+++ src/main/java/me/dio/domain/User.java
@@ -0,0 +1,4 @@
+package me.dio.domain;
+
+public class User {
+}
